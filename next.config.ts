2b4561a6e8c14a3586b8d8016f376ad30bcae232
Arnav Menon <arnavmenon@gmail.com>
2025-04-07 00:10:37 -0400

    fix linting errors

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,25 @@
+import type { NextConfig } from "next"
+import type { Configuration } from "webpack"
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
+const nextConfig: NextConfig = {
+  webpack: (config: Configuration, { isServer }: { isServer: boolean }) => {
     if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...config.resolve?.fallback,
+          fs: false,
+        },
       }
     }
 
     return config
   },
+
   env: {
     LOGIN_REQUIRED: process.env.LOGIN_REQUIRED,
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
